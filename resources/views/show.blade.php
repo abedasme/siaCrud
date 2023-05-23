1526bfae69d3d3779086512678cc72db449b3d0d
unknown <fordotes01@gmail.com>
2023-05-23 23:14:26 +0800

    uncompleted navbar

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,7 +3,21 @@
 @section('content')
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-<h1 class="page-header text-center">Angelo</h1>
+<nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Capbois</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup ">
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+        <a class="nav-link" href="#">Data Tables</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
 <div class="row">
     <div class="col-md-12 col-md-offset-1">
         <h3>Members Table
