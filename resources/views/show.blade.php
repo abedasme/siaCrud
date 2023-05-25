f524168c127b58e27b00d87609e1585c5bbdb0aa
unknown <jbcasininllo38@gmail.com>
2023-05-25 12:54:59 +0800

    print button added

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,9 +9,9 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup ">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav ms-auto">
-        <a class="nav-link" href="#">Data Tables</a>
+        <a class="nav-link" href="#">Log out</a>
       </div>
     </div>
   </div>
@@ -31,11 +31,10 @@
             <input type="text" name="search" class="form-control me-2" placeholder="Search members">
             <button type="submit" class="btn btn-xs btn-primary">Search</button>
             <a href="{{ route('members.index') }}" class="btn btn-sm btn-secondary ms-2">Show All</a>
+            <button class="btn btn-success ms-2" onclick="window.print()"><i class="fa fa-print"></i> Print</button> <!-- Print button -->
         </form>
     </div>
 </div>
-
-
 
 <div class="row">
     <div class="col-md-12 col-md-offset-1">
